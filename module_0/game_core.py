2c2cbd7c58c06b6b5a43ddd179b0a1a38d332010
DmitryKhoteenkov <dhoteenkov@gmail.com>
2020-07-28 21:37:25 +0300

    commit_0

---
+++ module_0/game_core.py
@@ -0,0 +1,45 @@
+﻿import numpy as np
+
+
+def game_core_DH(number):
+    '''Функция принимает загаданное число в диапазоне от 1 до 100
+
+    и возвращает число попыток
+
+    '''
+
+    count = 1
+    left_num = 1  # левая граница диапазона включительно
+    right_num = 100  # правая граница диапазона включительно
+    predict = (left_num+right_num)//2  # предполагаемое число
+
+    while number != predict:
+        count += 1
+        predict = (left_num+right_num)//2
+        if number > predict:
+            left_num = predict + 1
+        else:
+            right_num = predict
+
+    return(count)
+
+
+def score_game(game_core):
+    '''Проверяем все возможные числа из диапазона от 1 до 100,
+
+    чтобы узнать, как быстро игра угадывает число
+
+    '''
+
+    count_ls = []
+
+    for number in range(1, 101):
+        count_ls.append(game_core(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+
+    return(score)
+
+
+score_game(game_core_DH)  # запускаем
+
